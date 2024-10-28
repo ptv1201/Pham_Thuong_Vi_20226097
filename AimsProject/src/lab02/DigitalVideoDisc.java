281e1fa86dd45b767d53c5ec955534bc7fb3c3e5
ptv1201 <phamthuongvi1201@gmail.com>
2024-10-29 00:09:02 +0700

    lab02_20226097

--- AimsProject/src/lab02/DigitalVideoDisc.java
+++ AimsProject/src/lab02/DigitalVideoDisc.java
@@ -6,6 +6,30 @@ private String category;
 private String director;
 private int length;
 private float cost;
+
+public DigitalVideoDisc(String title) {
+    super();
+    this.title = title;
+}
+public DigitalVideoDisc(String title, String category, float cost) {
+    this.title = title;
+    this.category = category;
+    this.cost = cost;
+}
+public DigitalVideoDisc(String title, String category, String director, float cost) {
+    this.title = title;
+    this.category = category;
+    this.director = director;
+    this.cost = cost;
+}
+public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+    this.title = title;
+    this.category = category;
+    this.director = director;
+    this.length = length;
+    this.cost = cost;
+}
+
 public String getTitle() {
 	return title;
 }
@@ -21,8 +45,5 @@ public int getLength() {
 public float getCost() {
 	return cost;
 }
-public DigitalVideoDisc(String title) {
-	super();
-	this.title = title;
-}
+
 }
