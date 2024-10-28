70157871430aaabcf8fd9c9fafc66937b81de569
ptv1201 <phamthuongvi1201@gmail.com>
2024-10-28 23:49:35 +0700

    lab02_20226097

---
+++ AimsProject/src/lab02/Aims.java
@@ -0,0 +1,10 @@
+package lab02;
+
+public class Aims {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AimsProject/src/lab02/Cart.java
@@ -0,0 +1,6 @@
+package lab02;
+
+public class Cart {
+public static final int MAX_NUMBERS_ORDERED=20;
+private DigitalVideoDisc itemsOrdered[]=new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+}
